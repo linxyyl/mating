4d5c3495fed4044d0769cdf13a889ebba6059d3c
drizzle <linxyyl@163.com>
2020-11-25 18:12:41 +0800

    V1.0版本

--- src/Mating.php
+++ src/Mating.php
@@ -9,33 +9,112 @@ namespace linxyyl\mating;
 
 class Mating
 {
+
+    const MAX_LENGTH = 1024;
+
+    /**
+     * 返回大写字符串
+     * @param int $length
+     * @return string
+     */
+    public static function upperString(int $length = 32)
+    {
+        $length = static::verifyLength($length);
+        return strtoupper(substr(static::getLetters(), 0, $length));
+    }
+
+    /**
+     * 返回小写字符串
+     * @param int $length
+     * @return string
+     */
+    public static function lowerString(int $length = 32)
+    {
+        $length = static::verifyLength($length);
+        return strtolower(substr(static::getLetters(), 0, $length));
+    }
+
+    /**
+     * 返回纯数字字符串
+     * @param int $length
+     * @return bool|string
+     */
+    public static function numberString(int $length = 32)
+    {
+        $length = static::verifyLength($length);
+        return substr(static::getNumber(), 0, $length);
+    }
+
+    /**
+     * 生成一个很长的字符串,可以用来制造垃圾
+     * @param int $length
+     * @return bool|string
+     */
+    public static function superString(int $length = 32)
+    {
+        if ($length > 9522) {
+            $length = 9522;
+        }
+
+        return substr(static::getSuper(), 0, $length);
+    }
+
+    private static function verifyLength(int $length)
+    {
+        if ($length > static::MAX_LENGTH) {
+            return static::MAX_LENGTH;
+        } else {
+            return $length;
+        }
+    }
+
+    private static function getLetters()
+    {
+        $str = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm';
+        $letters = '';
+        for ($i = 0; $i <= 50; $i++) {
+            $letters .= str_shuffle($str);
+        }
+        return $letters;
+    }
+
+    private static function getNumber()
+    {
+        $str = '01234567899876543210';
+        $letters = '';
+        for ($i = 0; $i <= 100; $i++) {
+            $letters .= str_shuffle($str);
+        }
+        return $letters;
+    }
+
     /**
      * @param int $length
      * @return string
      */
-    public static  function getStr(int $length = 64): string
+    private static function getSuper(): string
     {
-        if ($length > 256) {
-            $length = 256;
-        }
 
         $ele = [
             'ele_1 ' => md5(uniqid()),//32
             'ele_1 ' => strtoupper(uniqid()),//32
-
             'ele_2 ' => md5(microtime()),//32
             'ele_2 ' => strtoupper(md5(microtime())),//32
-
             'ele_3' => md5(uniqid()),//32
             'ele_3' => strtoupper(md5(uniqid())),//32
-
             'ele_4 ' => substr(str_shuffle('QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'), 0, 32),//32
             'ele_6 ' => md5(str_shuffle('1234567890')),// 32
-
             'ele_7 ' => str_shuffle('[@$%*(=]!'),// 9
             'ele_8' => substr(password_hash(md5(uniqid()), PASSWORD_BCRYPT), 32), //32
+            'ele_9' => self::upperString(1024),
+            'ele_9' => self::lowerString(1024),
+            'ele_9' => self::numberString(1024),
+            'ele_9' => self::getLetters(),
+            'ele_10' => self::getNumber(),
+            'ele_11' => self::getLetters(),
+            'ele_12' => self::getNumber(),
         ];
-
-        return substr(str_shuffle(implode('', $ele)), 0, $length);
+        
+        return str_shuffle(implode('', $ele));
     }
 }
